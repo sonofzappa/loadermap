6202ffc268bfb482db24769dd75f82832c1ae9ec
sonofzappa <shadowsofhistory@protonmail.com>
2021-07-24 11:13:41 -0500

    seed update 2

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,16 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
   }
+
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
+const Loader = require('../models/loader');
+const { captureRejectionSymbol } = require('events');
+const faker = require('faker');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
-  mongoose.connect('mongodb://localhost:27017/loader-app', {
+mongoose.connect('mongodb://localhost:27017/loader-app', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -16,12 +23,6 @@ db.once("open", () => {
     console.log("Database connected")
 })
 
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
-const Loader = require('../models/loader');
-const { captureRejectionSymbol } = require('events');
-const faker = require('faker');
 
 mongoose.connect('mongodb://localhost:27017/loader-app', {
     useNewUrlParser: true,
@@ -29,11 +30,9 @@ mongoose.connect('mongodb://localhost:27017/loader-app', {
     useUnifiedTopology: true
 });
 
-
-
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async() => {
+const seedDB = async () => {
     await Loader.deleteMany({});
     for (let i = 0; i < 300; i++){
         const random1000 = Math.floor(Math.random() * 1000);
@@ -63,41 +62,3 @@ const seedDB = async() => {
 seedDB().then(() => {
     mongoose.connection.close();
 });
-
-// const mongoose = require('mongoose');
-// const cities = require('./cities');
-// const {places, descriptors} = require('./seedHelpers');
-// const Loader = require('../models/Loader');
-// const { captureRejectionSymbol } = require('events');
-
-// mongoose.connect('mongodb://localhost:27017/loader-app', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// });
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", () => {
-//     console.log("Database connected")
-// })
-
-// const sample = array => array[Math.floor(Math.random() * array.length)];
-
-// const seedDB = async () => {
-//     await Loader.deleteMany({});
-//     for (let i = 0; i < 15; i++){
-//         const random1000 = Math.floor(Math.random() * 1000);
-//         const surcharge = Math.random() * 5;
-//         const loader = new Loader({
-//             author: '60f1911246574634b8d47ea4',
-//             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-//             name: `${sample(descriptors)} ${sample(places)}`,
-//         })
-//         await loader.save()
-//     }
-// }
-
-// seedDB().then(() => {
-//     mongoose.connection.close();
-// });
